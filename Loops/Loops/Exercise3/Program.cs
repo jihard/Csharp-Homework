773af68815cd813716c442ee30e3d732ed16e390
Jihard@inbox.lv <Jihard@inbox.lv>
2021-08-02 03:31:35 +0300

    Loop

--- Loops/Loops/Exercise3/Program.cs
+++ Loops/Loops/Exercise3/Program.cs
@@ -4,7 +4,7 @@ namespace Exercise3
 {
     class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
             int[] emptyArr = new int[20];
             int arrylenght = emptyArr.Length;
