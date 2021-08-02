942e23a303a53f771fbe6f28cf438eb01fd65e08
Jihard@inbox.lv <Jihard@inbox.lv>
2021-08-02 03:55:09 +0300

    Loop

---
+++ Loops/Loops/Exercise7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerRandomNum;
+            int playerPoints = 1 * 7;
+            int playerPoints1 = 0;
+            Console.WriteLine("Welcome to Dice game");
+            Console.WriteLine("--------------------");
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                string roll;
+                do
+                {
+                    playerRandomNum = random.Next(1, 7);
+                    playerPoints++;
+                    Console.WriteLine("you rolled a " + playerRandomNum);
+                    if (playerRandomNum < 2)
+                    {
+                        Console.WriteLine("Your pointas are " + playerPoints1);
+                        break;
+                    }
+                    Console.Write("Do you wanna roll again? (Y/N) -- ");
+                    roll = Console.ReadLine();
+                    if (roll.Equals("Y"))
+                    {
+                        continue;
+                    }
+                    else if (roll.Equals("N"))
+                    {
+                        Console.WriteLine("your pints are " + playerPoints);
+                    }
+                } while (roll == "Y");
+                break;
+            }
+        }
+    }
+}
---
+++ Loops/Loops/Exercise9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Desired dice sum: ");
+            int sum = Int32.Parse(Console.ReadLine());
+            Random random = new Random();
+            int dice1 = random.Next(6) + 1;
+            int dice2 = random.Next(6) + 1;
+            int diceSum = dice1 + dice2;
+            Console.WriteLine(dice1 + " and " + dice2 + " = " + diceSum);
+            while (diceSum != sum)
+            {
+                dice1 = random.Next(6) + 1;
+                dice2 = random.Next(6) + 1;
+                diceSum = dice1 + dice2;
+                Console.WriteLine(dice1 + " and " + dice2 + " = " + diceSum);
+            }
+        }
+    }
+}
