8589e9cebd8054a3b406894b694a4c7f6ea4f6c7
Jihard@inbox.lv <Jihard@inbox.lv>
2021-08-10 12:33:13 +0300

    Classes

--- ClassesAndObjects/Classes4/Program.cs
+++ ClassesAndObjects/Classes4/Program.cs
@@ -2,28 +2,28 @@
 
 namespace Classes4
 {
-	class Program
+    class Program
     {
-		static void Main(string[] args)
+	    static void Main(string[] args)
         {
-			Movie movie = new Movie("PG-13", "Eon Productions", "Casino Royale");
-			Movie movie1 = new Movie("PG-13", "Buena Vista International", "Glass");
-			Movie movie2 = new Movie("PG", "Columbia Pictures", "Spider-Man: Into the Spider-Verse");
-			Console.Write("The movie title is " + movie.GetTitle());
-			Console.Write(", the studio " + movie.Getstudio());
-			Console.Write(", and the rating " + movie.GetRating());
-			Console.WriteLine("\n");
-			Console.WriteLine("-----------------------------------");
-			Console.Write("The movie title is " + movie1.GetTitle());
-			Console.Write(", the studio " + movie1.Getstudio());
-			Console.Write(", and the rating " + movie1.GetRating());
+		    Movie movie = new Movie("PG-13", "Eon Productions", "Casino Royale");
+	        Movie movie1 = new Movie("PG-13", "Buena Vista International", "Glass");
+	        Movie movie2 = new Movie("PG", "Columbia Pictures", "Spider-Man: Into the Spider-Verse");
+		    Console.Write("The movie title is " + movie.GetTitle());
+		    Console.Write(", the studio " + movie.Getstudio());
+		    Console.Write(", and the rating " + movie.GetRating());
 		    Console.WriteLine("\n");
-			Console.WriteLine("-----------------------------------");
-			Console.Write("The movie title is " + movie2.GetTitle());
-			Console.Write(", the studio " + movie2.Getstudio());
-			Console.Write(", and the rating " + movie2.GetRating());
-			Console.WriteLine("\n");
-			Console.WriteLine("-----------------------------------");
-		}
-	}
+	        Console.WriteLine("-----------------------------------");
+		    Console.Write("The movie title is " + movie1.GetTitle());
+	        Console.Write(", the studio " + movie1.Getstudio());
+	        Console.Write(", and the rating " + movie1.GetRating());
+		    Console.WriteLine("\n");
+	        Console.WriteLine("-----------------------------------");
+		    Console.Write("The movie title is " + movie2.GetTitle());
+            Console.Write(", the studio " + movie2.Getstudio());
+	        Console.Write(", and the rating " + movie2.GetRating());
+            Console.WriteLine("\n");
+            Console.WriteLine("-----------------------------------");
+	    }
+    }
 }
