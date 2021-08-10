06590af1fb03ab05f6781955742629815451a784
Jihard@inbox.lv <Jihard@inbox.lv>
2021-08-10 08:22:58 +0300

    Classes

---
+++ ClassesAndObjects/Classes1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Classes1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product("Logitech mouse", (decimal)70.00, 14);
+            Product product2 = new Product("iPhone 5s", (decimal)999.99, 3);
+            Product product3 = new Product("Epson EB-U05", (decimal)440.46, 1);
+            product1.PrintProduct();
+            product2.PrintProduct();
+            product3.PrintProduct();
+            Console.WriteLine("\n");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("New product parametrs");
+            Console.WriteLine("--------------------------------");
+            product1.ChangePrice((decimal)30.0);
+            product1.ReduceAmount();
+            product1.PrintProduct();
+            product2.ChangePrice((decimal)500.99);
+            product2.ReduceAmount();
+            product2.PrintProduct();
+            product3.ChangePrice((decimal)200.0);
+            product3.ReduceAmount();
+            product3.PrintProduct();
+        }
+    }
+}
---
+++ ClassesAndObjects/Classes2/Odometer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes2
+{
+    class Odometer
+    {
+        public Odometer(FuelGauge fuelGauge)
+        {
+            _fuelGauge = fuelGauge;
+        }
+
+        private readonly FuelGauge _fuelGauge;
+        private int _currentMilage;
+
+        public int Report()
+        {
+            return _currentMilage;
+        }
+        public void Increment()
+        {
+            if (_currentMilage < 999999)
+            {
+                _currentMilage++;
+                if (_currentMilage % 10 == 0)
+                    _fuelGauge.DecreseLevel();
+            }
+            else
+            {
+                _currentMilage = 0;
+            }
+        }
+    }
+}
