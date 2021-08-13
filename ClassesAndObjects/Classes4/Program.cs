24bed043b67156c0a4b0561a7e79513683ca8908
Jihard@inbox.lv <Jihard@inbox.lv>
2021-08-13 03:50:34 +0300

    Class

--- ClassesAndObjects/Classes4/Program.cs
+++ ClassesAndObjects/Classes4/Program.cs
@@ -7,8 +7,23 @@ namespace Classes4
         static void Main(string[] args)
         {
             Movie movie = new Movie("PG-13", "Eon Production","Casino Royale");
-            movie movie1 = new Movie("PG-13", "Buena Vista International","Glass");
-            movie movie2 = new movie("PG", "Columbia Pictures", "Spider-Man: Into the Spider-Verse");
+            Movie movie1 = new Movie("PG-13", "Buena Vista International","Glass");
+            Movie movie2 = new Movie("PG", "Columbia Pictures", "Spider-Man: Into the Spider-Verse");
+            Console.Write("The movie title is "+movie.GetTitle());
+            Console.Write(", the studio "+movie.Getstudio());
+            Console.Write(", and the rating "+movie.GetRating());
+            Console.WriteLine("\n");
+            Console.WriteLine("----------------------------------");
+            Console.Write("The movie title is "+movie1.GetTitle());
+            Console.Write(", the studio "+movie1.Getstudio());
+            Console.Write(", and the rating "+movie1.GerRating());
+            Console.WriteLine("\n");
+            Console.WriteLine("-----------------------------------");
+            Console.Write("The movie title is "+movie2.GetTitle());
+            Console.Write(", the studio "+movie2.Getstudio());
+            Console.Write(", and the rating "+movie2.GetRating());
+            Console.WriteLine("\n");
+            Console.WriteLine("-----------------------------------");
         }
     }
 }
