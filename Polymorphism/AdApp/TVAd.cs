ffae450da4434fdcd317b078291ca3407b909de7
Jihard@inbox.lv <Jihard@inbox.lv>
2021-08-24 16:22:51 +0300

    Polymorphism

--- Polymorphism/AdApp/TVAd.cs
+++ Polymorphism/AdApp/TVAd.cs
@@ -2,16 +2,27 @@ namespace AdApp
 {
     public class TVAd: Advert
     {
-        public TVAd(int fee) : base(fee)
+        private int _airTime;
+        private int _peakTime;
+        private bool _hasPeakTime;
+
+        public TVAd(int fee, int airTime, int peakTime, bool hasPeakTime = false) : base(fee)
         {
-        }
-        
-        public new int Cost() 
-        {
-            return base.Cost();
+            _hasPeakTime = hasPeakTime;
+            _airTime = airTime;
+            _peakTime = peakTime;
         }
 
-        public override string ToString() 
+        public new int Cost()
+        {
+            var fee = base.Cost();
+            var cost = (_airTime - _peakTime)* fee;
+            if (_hasPeakTime)
+                cost += fee * _peakTime * 2;
+            return cost;
+        }
+
+        public override string ToString()
         {
             return base.ToString();
         }
